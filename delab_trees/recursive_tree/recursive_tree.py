258b31a50cef05fefdbe56d82da743b43aa605e1
Julian Dehne <julian.dehne@gmail.com>
2023-07-02 15:01:36 +0200

    - refactored recursive_tree to include tree_id (for compatibility with DelabTree naming - implemented reddit_downloader

--- delab_trees/recursive_tree/recursive_tree.py
+++ delab_trees/recursive_tree/recursive_tree.py
@@ -6,7 +6,8 @@ import xml.etree.ElementTree as ET
 
 
 class TreeNode:
-    def __init__(self, data, node_id, parent_id=None, parent_type="replied_to", tree_id_name="conversation_id"):
+    def __init__(self, data, node_id, parent_id=None, tree_id=None, parent_type="replied_to",
+                 tree_id_name="conversation_id"):
         """data is a tweet's json object
            tree_id is the logical id of the treenode (either author id when downloading, or twitter id in db)
            parent references the tree_id of the parent
@@ -16,25 +17,14 @@ class TreeNode:
         if not custom_is_iterable(data):
             logger.error("data should be iterable and contain 'tree_id' and 'post_id' as well as 'text' as keys")
         else:
-            if tree_id_name not in data:
-                logger.error(f"tree_id_name: {tree_id_name} should be in the dataset or changed as param for TreeNode")
+            if tree_id is None:
+                if tree_id_name not in data:
+                    logger.error(
+                        f"tree_id_name: {tree_id_name} should be in the dataset or changed as param for TreeNode")
+                else:
+                    self.tree_id = data[tree_id_name]
             else:
-                self.tree_id = data[tree_id_name]
-        """
-        the following data fields are required
-          text= root_node.data["text"],
-          simple_request=simple_request,
-          twitter_id=root_node.data["id"],
-          author_id=root_node.data["author_id"],
-          conversation_id=conversation_id,
-          created_at=root_node.data["created_at"],
-          in_reply_to_user_id=root_node.data.get("in_reply_to_user_id", None),
-          in_reply_to_status_id=root_node.data.get("in_reply_to_status_id", None),
-          tn_parent_id=tn_parent,
-          # tn_priority=priority,
-          language=root_node.data["lang"])
-
-        """
+                self.tree_id = tree_id
 
         self.children = []
         self.max_path_length = 0
