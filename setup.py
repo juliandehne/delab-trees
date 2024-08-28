7939f18b6df3be55dabf1e247df40c26ebb6824d
Julian Dehne <julian.dehne@gmail.com>
2024-08-28 15:22:53 +0200

    - added build-tools

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="delab-trees",
-    version="0.4.0",
+    version="0.4.1",
     description="a library to analyse reply trees in forums and social media",
     long_description=long_description,
     long_description_content_type="text/markdown",
